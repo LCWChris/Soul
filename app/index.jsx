9c56df65c99fd7ee053920b68fe4c75b58cbb25d
陳彥友 <2791163@gmail.com>
2025-08-04 16:45:35 +0800

    新增使用者偏好的頁面，並微調app/index，判斷問卷是否已經填答

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,43 @@
+import { useAuth } from "@clerk/clerk-expo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
-export default function index() {
-  return <Redirect href="/(auth)/sign-in" />;
+export default function Index() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasFilled, setHasFilled] = useState(false);
+  const { isSignedIn, user } = useAuth();
+
+  useEffect(() => {
+    const checkQuestionnaire = async () => {
+      try {
+        if (user?.id) {
+          const filled = await AsyncStorage.getItem(`questionnaireFilled_${user.id}`);
+          console.log("問卷紀錄值：", filled);
+          setHasFilled(filled === "true");
+        }
+      } catch (e) {
+        console.error("讀取問卷狀態失敗", e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkQuestionnaire();
+  }, [user]);
+
+  if (!isSignedIn) {
+    return <Redirect href="/(auth)/sign-in" />;
+  }
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return hasFilled ? <Redirect href="/(tabs)" /> : <Redirect href="/onboarding/preference" />;
 }
